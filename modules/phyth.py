ab208fbc829c3e70d1a541ea2cb225a09bf29b78
EC2 Default User <ec2-user@ip-172-31-43-6.us-west-2.compute.internal>
2015-02-01 19:59:40 +0000

    added global response method

--- modules/phyth.py
+++ modules/phyth.py
@@ -6,5 +6,15 @@ def getData():
 	data = json.loads(json_data)
 	return data['data']
 
+def logError(error):
+	#causes response to be blank currently...
+	log = open('log.txt', 'w+')
+	log.write(error)
+	log.close()
+
+def respond(data_dict):
+	reply_data = { 'data': data_dict,'error': '' }
+	print reply_data
+
 
 
