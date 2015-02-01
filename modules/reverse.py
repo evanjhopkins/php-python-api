e422da37e73827adad674e4beaa013045daa1e5a
EC2 Default User <ec2-user@ip-172-31-43-6.us-west-2.compute.internal>
2015-02-01 20:00:15 +0000

    simplified code and added comments for example module

--- modules/reverse.py
+++ modules/reverse.py
@@ -1,8 +1,21 @@
+
 import phyth
 
 def main():
+	# dictionary of data sent in through api request
 	data = phyth.getData()
-	print data['string'][::-1]
+
+	# getting string to be reversed from data dictionary
+	string = data['string']
+
+	# reversing string
+	string = string[::-1]
+
+	# building reply. MUST be a dictionary
+	reply_data = { 'new_string': str(string) }
+
+	# send response
+	phyth.respond(reply_data)
 
 if __name__ == "__main__":
     main()
