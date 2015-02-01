c3f1a49c20b442aa1eaeb11bb1acd9f0a70ea1bf
EC2 Default User <ec2-user@ip-172-31-43-6.us-west-2.compute.internal>
2015-02-01 17:57:20 +0000

    condensing code

--- phyth.php
+++ phyth.php
@@ -1,16 +1,12 @@
 <?php
 if(isset($_POST['data'])){
-	//remove spaces from json
-	$json = json_decode($_POST['data']);
-	$datastring = $_POST['data'];
-	//$datastring = str_replace(" ", "\ ", $_POST['data']);
-	$datastring = "'".$datastring."'";
-	echo apiCall($json->cmd, $datastring);
+	$datastring = "'".$_POST['data']."'";
+	echo apiCall(json_decode($_POST['data'])->cmd, $datastring);
 }
 
 function apiCall($call, $data){	
 	$cmd = "python modules/".$call.".py"." ".$data;
 	$response = exec($cmd);
-	return $response;//test
+	return $response;
 }
 ?>
