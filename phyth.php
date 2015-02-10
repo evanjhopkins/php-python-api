3cc83252f6fa3591fd33a7e2159dc1cb64ef106e
EvanJHopkins <moviemaker7@gmail.com>
2015-02-10 03:16:13 +0000

    made the api function its own post var

--- phyth.php
+++ phyth.php
@@ -1,14 +1,12 @@
 <?php
-if(isset($_POST['data'])){
-	$datastring = "'".$_POST['data']."'";
-	echo apiCall(json_decode($_POST['data'])->cmd, $datastring);
-}else if(isset($_GET['data'])){
-	$datastring = "'".$_GET['data']."'";
-	echo apiCall(json_decode($_GET['data'])->cmd, $datastring);
+if(isset($_POST['function'])){
+	echo apiCall($_POST['function'],"'".$_POST['data']."'");
+}else if(isset($_GET['function'])){
+	echo apiCall($_GET['function'],"'".$_GET['data']."'");
 }
 
-function apiCall($call, $data){	
-	$cmd = "python modules/".$call.".py"." ".$data;
+function apiCall($function, $data){	
+	$cmd = "python modules/".$function.".py"." ".$data;
 	$response = exec($cmd);
 	return $response;
 }
