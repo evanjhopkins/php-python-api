bab94cf4ed43ae87c644a59ba5738634df94ab0e
EvanJHopkins <moviemaker7@gmail.com>
2015-02-13 02:12:39 +0000

    added file upload verification

--- modules/phyth.py
+++ modules/phyth.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import os
 
 def getData():
 	json_data = sys.argv[1]
@@ -28,10 +29,15 @@ def start(func):
 		respond("", "ERROR: Module failed; exception not handled by module")
 
 def verifyFile(file_data):
-	if (file_data['error'] == 0 and file_data['size'] > 0):
+	# verifies that the file has no errors (php) and has a at least one byte (php)
+	# verifies that the file exists in the phyth tmp dir (python)
+	
+	file_path = str(os.getcwd())+"/tmp/"+file_data['name']
+	if (file_data['error'] == 0 and file_data['size'] > 0 and os.path.isfile(file_path) ):
 		return True
 	else:
 		return False
+
 
 
 
