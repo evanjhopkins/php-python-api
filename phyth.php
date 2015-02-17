6ee66e9b7e041725cba0cc0f500b69da011ea749
EvanJHopkins <moviemaker7@gmail.com>
2015-02-17 03:06:15 +0000

    added back GET support

--- phyth.php
+++ phyth.php
@@ -3,9 +3,13 @@
 		$target = __DIR__."/tmp/".$file['name'];
 		move_uploaded_file($file['tmp_name'], $target);
 	}
-
-	$data = "'".json_encode(array_merge($_POST, $_FILES))."'";
-	echo apiCall($_POST['function'], $data);
+	if(isset($_POST['function'])){
+		$data = "'".json_encode(array_merge($_POST, $_FILES))."'";
+		echo apiCall($_POST['function'], $data);
+	}else if(isset($_GET['function'])){
+		$data = "'".json_encode(array_merge($_GET, $_FILES))."'";
+		echo apiCall($_GET['function'], $data);
+	}
 
 function apiCall($function, $data){	
 	$cmd = "python modules/".$function.".py"." ".$data;
